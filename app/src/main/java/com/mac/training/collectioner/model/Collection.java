5f1a04779af8b5a24660efdf66c16f4776f07c4d
Jaime Santiesteban <jaimesantiesteban07@gmail.com>
2016-09-19 18:21:40 -0400

    Added main functionality for add collection

---
+++ app/src/main/java/com/mac/training/collectioner/model/Collection.java
@@ -0,0 +1,42 @@
+package com.mac.training.collectioner.model;
+
+/**
+ * Created by User on 9/19/2016.
+ */
+public class Collection {
+
+    String name;
+    String description;
+
+    public Collection() {
+    }
+
+    public Collection(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Collection{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
