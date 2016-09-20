c3539c9cb7a407eb2e104762d1d80f0681bb4fb8
Josimar Vazquez <Josimar Vazquez>
2016-09-19 20:30:47 -0400

    CRUD Categories, no update yet

--- app/src/main/java/com/mac/training/collectioner/FirebaseCollectionsController.java
+++ app/src/main/java/com/mac/training/collectioner/FirebaseCollectionsController.java
@@ -8,36 +8,57 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.mac.training.collectioner.model.Collection;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FirebaseCollectionsController {
 
     static private DatabaseReference mDatabase;
-    static ArrayList<String> users;
-
-
+    //static ArrayList<Collection> users;
 
     static {
         mDatabase= FirebaseDatabase.getInstance().getReference();
     }
 
+    static public void insertCollection(String user, String collectionName){
+        String key = mDatabase.child(user).push().getKey();
+
+        Collection c = new Collection(collectionName);
+
+        Map<String, Object> cValues = c.toMap();
+        Map<String, Object> childUpdates = new HashMap<>();
+
+        childUpdates.put(user+"/"+key , cValues);
+
+        mDatabase.updateChildren(childUpdates);
+    }
 
 
-    public static ArrayList<String> getUsers(){
+    public static void deleteUserCollection(String user, String collectionName){
+        mDatabase
+                .child(user)
+                .child(collectionName)
+                .removeValue();
+    }
+
+
+    public static void getUserCollections(String user){
 
 
         Query recentPostsQuery = mDatabase
+                .child(user)
                 .limitToFirst(100);
-
-         users = new ArrayList<>();
 
         recentPostsQuery.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String c = dataSnapshot.getValue(String.class);
-                users.add(c);
+                Collection c = dataSnapshot.getValue(Collection.class);
+                Log.d("wwedwhieud", c.collection);
+                Log.d("wwedwhieud", dataSnapshot.getKey());
             }
 
             @Override
@@ -61,8 +82,9 @@ public class FirebaseCollectionsController {
             }
         });
 
-        return users;
+
     }
+
 
 
 
--- app/src/main/java/com/mac/training/collectioner/JosTest.java
+++ app/src/main/java/com/mac/training/collectioner/JosTest.java
@@ -4,20 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.mac.training.collectioner.dummy.Dummy;
-import com.mac.training.collectioner.model.Collection;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JosTest extends AppCompatActivity {
 
@@ -33,41 +23,28 @@ public class JosTest extends AppCompatActivity {
     }
 
 
-    public void insertCategories(View view) {
-
-        ArrayList<String> asd = FirebaseCollectionsController.getUsers();
-
-        for(String s : asd)
-            Log.d(TAG, s);
-
-
-       /* String user = Dummy.genUser();
+    public void insertCollection(View view) {
+        String user = Dummy.genUser();
         String collection = Dummy.genCollection();
 
-        String key = mDatabase.child(user).push().getKey();
+        FirebaseCollectionsController.insertCollection(user, collection);
 
-        //Item i = new Item("name", "description", 12.0, 15.0, "location", "used", "img-url");
-        Collection c = new Collection(collection);
+    }
+    public void deleteCollection(View view) {
 
-        Map<String, Object> cValues = c.toMap();
-        Map<String, Object> childUpdates = new HashMap<>();
+        String user = ((EditText)findViewById(R.id.user)).getText().toString();
+        String collection = ((EditText)findViewById(R.id.colKey)).getText().toString();
 
-        childUpdates.put(user+"/"+key , cValues);
+        FirebaseCollectionsController.deleteUserCollection(user, collection);
 
-        mDatabase.updateChildren(childUpdates);*/
     }
 
 
+    public void queryCollections(View view) {
 
+        String user = ((EditText)findViewById(R.id.user)).getText().toString();
+        Log.d(TAG, user+"------------------");
+         FirebaseCollectionsController.getUserCollections(user);
 
-
-
-
-
-
-
-
-
-
-
+    }
 }
