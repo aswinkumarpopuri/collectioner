b7c1b9bdb6718d619f2052cdb1a6b61481b221e5
Jaime Santiesteban <jaimesantiesteban07@gmail.com>
2016-09-19 23:10:10 -0400

    Added delete, modify, and view collection functionality

--- app/src/main/java/com/mac/training/collectioner/activity/ViewCollectionActivity.java
+++ app/src/main/java/com/mac/training/collectioner/activity/ViewCollectionActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mac.training.collectioner.R;
 import com.mac.training.collectioner.adapter.CollectionAdapter;
+import com.mac.training.collectioner.adapter.helper.SimpleItemTouchHelperCallback;
 import com.mac.training.collectioner.model.Collection;
 
 import java.util.ArrayList;
@@ -18,8 +20,9 @@ import java.util.List;
 public class ViewCollectionActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private CollectionAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private ItemTouchHelper mItemTouchHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +35,22 @@ public class ViewCollectionActivity extends AppCompatActivity {
         // in content do not change the layout size of the RecyclerView
         mRecyclerView.setHasFixedSize(true);
 
+        // specify an adapter (see also next example)
+        List<Collection> collectionList = new ArrayList<Collection>();
+        collectionList.add(new Collection());
+        collectionList.add(new Collection());
+        mAdapter = new CollectionAdapter(collectionList, this);
+        mRecyclerView.setAdapter(mAdapter);
+
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        // specify an adapter (see also next example)
-        List<Collection> collectionList = new ArrayList<Collection>();
-        collectionList.add(new Collection());
-        mAdapter = new CollectionAdapter(collectionList);
-        mRecyclerView.setAdapter(mAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+
     }
 
     // Add collection
--- app/src/main/java/com/mac/training/collectioner/model/Collection.java
+++ app/src/main/java/com/mac/training/collectioner/model/Collection.java
@@ -1,5 +1,8 @@
 package com.mac.training.collectioner.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.firebase.database.Exclude;
 
 import java.util.HashMap;
@@ -8,14 +11,27 @@ import java.util.Map;
 /**
  * Created by User on 9/19/2016.
  */
-public class Collection {
+public class Collection implements Parcelable {
 
     public String collection;
 
-    public Collection(){}
+    public Collection() {
+    }
 
     public Collection(String name) {
         this.collection = name;
+    }
+
+    protected Collection(Parcel in) {
+        this.collection = in.readString();
+    }
+
+    public String getCollection() {
+        return collection;
+    }
+
+    public void setCollection(String collection) {
+        this.collection = collection;
     }
 
     @Exclude
@@ -24,4 +40,27 @@ public class Collection {
         result.put("collection", collection);
         return result;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.collection);
+    }
+
+    public static final Creator<Collection> CREATOR = new Creator<Collection>() {
+        @Override
+        public Collection createFromParcel(Parcel in) {
+            return new Collection(in);
+        }
+
+        @Override
+        public Collection[] newArray(int size) {
+            return new Collection[size];
+        }
+    };
+
 }
