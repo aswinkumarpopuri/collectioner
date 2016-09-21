5cdce5b6bb1ec186e3d01da971ca20042ab70670
Josimar Vazquez <Josimar Vazquez>
2016-09-20 21:33:04 -0400

    need a valid user in order to retrieve image

--- app/src/main/java/com/mac/training/collectioner/storage/Storage.java
+++ app/src/main/java/com/mac/training/collectioner/storage/Storage.java
@@ -1,12 +1,15 @@
 package com.mac.training.collectioner.storage;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -15,12 +18,19 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.mac.training.collectioner.R;
+import com.squareup.picasso.Picasso;
 
 public class Storage extends AppCompatActivity {
 
-    Button b;
+    Button bSelect;
+    ImageView imgV;
+
+
+    Button bUpload;
     StorageReference mStorage;
-    private static final int GALLERY_INTENT = 2;
+    private static final int GALLERY_REQUEST = 2;
+    private static final int CAMERA_REQUEST = 2;
+    private ProgressDialog  progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +39,25 @@ public class Storage extends AppCompatActivity {
 
         mStorage = FirebaseStorage.getInstance().getReference();
 
-        b = (Button) findViewById(R.id.selectImage);
 
-        b.setOnClickListener(new View.OnClickListener() {
+        bSelect = (Button) findViewById(R.id.selectImage);
+        bUpload = (Button) findViewById(R.id.uploadImage);
+        imgV = (ImageView) findViewById(R.id.imageStore);
 
+        bSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,CAMERA_REQUEST);
+            }
+        });
+
+        bUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(intent,GALLERY_INTENT);
+                startActivityForResult(intent,GALLERY_REQUEST);
             }
         });
 
@@ -47,19 +67,26 @@ public class Storage extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            progressDialog = new ProgressDialog(this);
+            if((requestCode == GALLERY_REQUEST)||(requestCode == CAMERA_REQUEST)){
+                progressDialog.setMessage("Loading..");
+                progressDialog.show();
+                Uri uri = data.getData();
+                StorageReference filepath = mStorage.child("Images").child("categoryId").child("imageId");
+                filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressDialog.dismiss();
 
-        if((requestCode == GALLERY_INTENT) && (resultCode == RESULT_OK)){
+                        Uri downloadUri = taskSnapshot.getDownloadUrl();
 
-            Uri uri = data.getData();
-            StorageReference filepath = mStorage.child("Images").child(uri.getLastPathSegment());
-            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(Storage.this, "liiiiiiiisto (8)", Toast.LENGTH_LONG).show();
-                }
-            });
+                        Picasso.with(Storage.this).load(downloadUri).fit().centerCrop().into(imgV);
 
-
+                        Toast.makeText(Storage.this, "Liiiistoo..! (8)", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
         }
     }
 }
